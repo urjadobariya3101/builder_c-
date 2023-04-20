e7e891dc5b30dff6ba00fc261eb2c1d3ecda09f4
urjadobariya3101 <115995504+urjadobariya3101@users.noreply.github.com>
2023-04-20 21:35:52 +0530

    Add files via upload

---
+++ state.cpp
@@ -0,0 +1,112 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+class States {
+
+	private:
+	static char state1[100];
+	static char state2[100];
+	static char state3[100];
+	static char state4[100];
+	static char state5[100];
+	static char state6[100];
+	static char state7[100];
+	static char state8[100];
+	static char state9[100];
+	static char state10[100];
+	static char state11[100];
+	static char state12[100];
+	static char state13[100];
+	static char state14[100];
+	static char state15[100];
+	static char state16[100];
+	static char state17[100];
+	static char state18[100];
+	static char state19[100];
+	static char state20[100];
+	static char state21[100];
+	static char state22[100];
+	static char state23[100];
+	static char state24[100];
+	static char state25[100];
+	static char state26[100];
+	static char state27[100];
+	static char state28[100];
+
+		public:
+			void getdata(){
+				int x;
+
+//				for(x=1;x<=28;x++){
+//						cout<<state[x];
+//				}
+                cout<<state1<<endl;
+                cout<<state2<<endl;
+                cout<<state3<<endl;
+                cout<<state4<<endl;
+                 cout<<state5<<endl;
+                cout<<state6<<endl;
+                cout<<state7<<endl;
+                cout<<state8<<endl;
+                cout<<state9<<endl;
+                cout<<state10<<endl;
+                 cout<<state11<<endl;
+                cout<<state12<<endl;
+                cout<<state13<<endl;
+                cout<<state14<<endl;
+                 cout<<state15<<endl;
+                cout<<state16<<endl;
+                cout<<state17<<endl;
+                cout<<state18<<endl;
+                 cout<<state19<<endl;
+                cout<<state20<<endl;
+                cout<<state21<<endl;
+                cout<<state22<<endl;
+                 cout<<state23<<endl;
+                cout<<state24<<endl;
+                cout<<state25<<endl;
+                cout<<state26<<endl;
+                cout<<state27<<endl;
+                cout<<state28<<endl;
+
+			};
+};
+char States ::  state1[100] = "Andhra Pradesh";
+char States ::  state2[100] = "Arunachal Pradesh";
+char States ::  state3[100] = "Assam";
+char States ::  state4[100] = "Bihar";
+char States ::  state5[100] = "Chhattisgarh";
+char States ::  state6[100] = "Goa";
+char States ::  state7[100] = "Gujarat";
+char States ::  state8[100] = "Haryana";
+char States ::  state9[100] = "Himachal Pradesh";
+char States ::  state10[100] = "Jharkhand";
+char States ::  state11[100] = "Karnataka";
+char States ::  state12[100] = "Kerala ";
+char States ::  state13[100] = "Madhya Pradesh";
+char States ::  state14[100] = "Maharashtra ";
+char States ::  state15[100] = "Manipur ";
+char States ::  state16[100] = "Meghalaya ";
+char States ::  state17[100] = "Mizoram ";
+char States ::  state18[100] = "Nagaland ";
+char States ::  state19[100] = "Odisha ";
+char States ::  state20[100] = "Punjab ";
+char States ::  state21[100] = "Rajasthan ";
+char States ::  state22[100] = "Sikkim ";
+char States ::  state23[100] = "Tamil Nadu ";
+char States ::  state24[100] = "Telangana ";
+char States ::  state25[100] = "Tripura ";
+char States ::  state26[100] = "Uttarakhand ";
+char States ::  state27[100] = "Uttar Pradesh ";
+char States ::  state28[100] = "West Bengal ";
+
+
+
+int main(){
+ States obj;
+ obj.getdata();
+
+	return 0;
+}
